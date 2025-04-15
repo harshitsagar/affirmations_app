8a422a412a3ae406786ac1d70ceab102a03b38c5
harshitsagar <https://github.com/harshitsagar>
2025-04-15 21:13:18 +0530

    completed app theme screen UI

--- lib/app/modules/screens/common/app_settings/app_themes/controllers/app_themes_controller.dart
+++ lib/app/modules/screens/common/app_settings/app_themes/controllers/app_themes_controller.dart
@@ -1,23 +1,77 @@
+import 'package:affirmations_app/app/data/components/images_path.dart';
+import 'package:affirmations_app/app/routes/app_pages.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
 class AppThemesController extends GetxController {
-  //TODO: Implement AppThemesController
+  final List<String> themeImages = [
+    theme1,
+    theme2,
+    theme3,
+    theme4,
+    theme5,
+    theme6,
+    theme7,
+    theme8,
+    theme9,
+  ];
 
-  final count = 0.obs;
+  // List to track which themes are locked (index-based)
+  final List<bool> lockedThemes = [
+    false, // first theme is unlocked
+    false, // second theme is unlocked
+    false, // third theme is unlocked
+    true,  // fourth theme is locked
+    true,  // fifth theme is locked
+    true,  // sixth theme is locked
+    true,  // seventh theme is locked
+    true,  // eighth theme is locked
+    true,  // ninth theme is locked
+  ];
+
+  // Observable selected theme index
+  var selectedTheme = 0.obs;
+
+  // Track premium status
+  var isPremium = false.obs;
+
+  // Hardcoded text colors for each theme (can be replaced with API data later)
+  final List<Color> themeTextColors = [
+    Colors.black,
+    Colors.black,
+    Colors.black,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.black,
+    Colors.black,
+    Colors.white,
+  ];
+
   @override
   void onInit() {
     super.onInit();
+    isPremium.value = false;
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void selectTheme(int index) {
+    if (isPremium.value || !lockedThemes[index]) {
+      selectedTheme.value = index;
+      update(); // This triggers the UI to refresh
+    } else {
+      Get.toNamed(Routes.SUBSCRIPTION_SCREEN);
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  void saveTheme() {
+    print('Selected theme: ${selectedTheme.value}');
+    Get.back();
   }
 
-  void increment() => count.value++;
+  // Method to update text colors when API is available
+  void updateTextColors(List<Color> colors) {
+    themeTextColors.clear();
+    themeTextColors.addAll(colors);
+    update();
+  }
 }
--- lib/app/modules/screens/common/app_settings/app_themes/views/app_themes_view.dart
+++ lib/app/modules/screens/common/app_settings/app_themes/views/app_themes_view.dart
@@ -1,22 +1,156 @@
+import 'package:affirmations_app/app/data/components/images_path.dart';
+import 'package:affirmations_app/app/widgets/customAppbar.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import '../controllers/app_themes_controller.dart';
 
 class AppThemesView extends GetView<AppThemesController> {
   const AppThemesView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('AppThemesView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'AppThemesView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(bgImage2),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(left: 20.w, right: 20.w, top: 10.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Custom App Bar with back button
+                CustomAppBar(title: "App Theme"),
+
+                SizedBox(height: 30.h),
+
+                // Title Text
+                Text(
+                  "Let's choose your theme",
+                  style: GoogleFonts.inter(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black,
+                  ),
+                ),
+
+                SizedBox(height: 32.h),
+
+                // Theme Grid
+                Expanded(
+                  child: GetBuilder<AppThemesController>(
+                    builder: (controller) {
+                      return GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 10.w,
+                          mainAxisSpacing: 10.h,
+                          childAspectRatio: 0.62,
+                        ),
+                        itemCount: controller.themeImages.length,
+                        itemBuilder: (context, index) {
+                          bool isSelected = controller.selectedTheme.value == index;
+                          bool isLocked = !controller.isPremium.value &&
+                              controller.lockedThemes[index];
+
+                          return GestureDetector(
+                            onTap: () => controller.selectTheme(index),
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                // Theme Container
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(12.r),
+                                    image: DecorationImage(
+                                      image: AssetImage(controller.themeImages[index]),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Aa",
+                                      style: GoogleFonts.inter(
+                                        fontSize: 25.sp,
+                                        fontWeight: FontWeight.w500,
+                                        color: controller.themeTextColors[index],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+
+                                // Selection indicator
+                                if (isSelected)
+                                  Positioned(
+                                    top: 8.h,
+                                    right: 8.w,
+                                    child: Image.asset(
+                                      greenTickIcon,
+                                      width: 30.w,
+                                      height: 30.h,
+                                    ),
+                                  ),
+
+                                // Lock overlay for locked themes
+                                if (isLocked)
+                                  Positioned(
+                                    top: -3.h,
+                                    right: 0.w,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12.r),
+                                      ),
+                                      child: Center(
+                                        child: Image.asset(
+                                          lockIcon,
+                                          width: 48.w,
+                                          height: 48.h,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+
+                // Save Button
+                Padding(
+                  padding: EdgeInsets.only(top: 16.h),
+                  child: ElevatedButton(
+                    onPressed: controller.saveTheme,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24.r),
+                      ),
+                      minimumSize: Size(double.infinity, 45.h),
+                    ),
+                    child: Text(
+                      "Save",
+                      style: GoogleFonts.inter(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
