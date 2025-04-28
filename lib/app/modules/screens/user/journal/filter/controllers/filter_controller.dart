60c83d1db7b517edf4ac3c46a287ccd31809a8a0
harshitsagar <https://github.com/harshitsagar>
2025-04-28 11:09:13 +0530

    created filter view screen for the selection of date range

--- lib/app/modules/screens/user/journal/filter/controllers/filter_controller.dart
+++ lib/app/modules/screens/user/journal/filter/controllers/filter_controller.dart
@@ -1,23 +1,92 @@
+import 'package:affirmations_app/app/modules/screens/user/journal/journal_home/controllers/journal_home_controller.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class FilterController extends GetxController {
-  //TODO: Implement FilterController
+  final fromDate = Rx<DateTime?>(null);
+  final toDate = Rx<DateTime?>(null);
+  final errorMessage = Rx<String?>(null);
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    // Initialize with default dates (last 7 days)
+    // final now = DateTime.now();
+    // toDate.value = now;
+    // fromDate.value = now.subtract(const Duration(days: 6));
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Future<void> selectFromDate(BuildContext context) async {
+    final selected = await showDatePicker(
+      context: context,
+      initialDate: fromDate.value ?? DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    );
+
+    if (selected != null) {
+      fromDate.value = selected;
+      _validateDates();
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  Future<void> selectToDate(BuildContext context) async {
+    final selected = await showDatePicker(
+      context: context,
+      initialDate: toDate.value ?? DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    );
+
+    if (selected != null) {
+      toDate.value = selected;
+      _validateDates();
+    }
   }
 
-  void increment() => count.value++;
+  void _validateDates() {
+    if (fromDate.value != null && toDate.value != null) {
+      if (fromDate.value!.isAfter(toDate.value!)) {
+        errorMessage.value = 'From date cannot be after To date';
+      } else if (toDate.value!.difference(fromDate.value!).inDays < 1) {
+        errorMessage.value = 'Minimum range of 2 days is required';
+      } else if (toDate.value!.difference(fromDate.value!).inDays > 6) {
+        errorMessage.value = 'Maximum range of 7 days is allowed';
+      } else {
+        errorMessage.value = null;
+      }
+    }
+  }
+
+  void applyFilters() {
+    _validateDates();
+
+    if (errorMessage.value != null) {
+      Get.snackbar('Error', errorMessage.value!,
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
+      return;
+    }
+
+    // Format the date range text
+    if (fromDate.value != null && toDate.value != null) {
+      final journalController = Get.find<JournalHomeController>();
+
+      // Update the date range text
+      if (fromDate.value!.month != toDate.value!.month) {
+        journalController.dateRangeText.value =
+        '${DateFormat('dd MMM').format(fromDate.value!)} - ${DateFormat('dd MMM yyyy').format(toDate.value!)}';
+      } else {
+        journalController.dateRangeText.value =
+        '${DateFormat('dd').format(fromDate.value!)} - ${DateFormat('dd MMM yyyy').format(toDate.value!)}';
+      }
+
+      // Generate chart data for the selected range
+      journalController.generateChartDataForRange(fromDate.value!, toDate.value!);
+    }
+
+    Get.back();
+  }
 }
--- lib/app/modules/screens/user/journal/filter/views/filter_view.dart
+++ lib/app/modules/screens/user/journal/filter/views/filter_view.dart
@@ -1,24 +1,183 @@
+import 'package:affirmations_app/app/data/components/images_path.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import '../controllers/filter_controller.dart';
 
 class FilterView extends GetView<FilterController> {
   const FilterView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('FilterView'),
-        centerTitle: true,
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFFFDF9F8),
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
       ),
-      body: const Center(
-        child: Text(
-          'FilterView is working',
-          style: TextStyle(fontSize: 20),
+      padding: EdgeInsets.only(left: 20.w, right: 20.w, top: 20.h, bottom: 40.h),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          GestureDetector(
+            onTap: () => Get.back(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+
+                Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.black,
+                  size: 20.sp,
+                ),
+
+                Text(
+                  'Back',
+                  style: GoogleFonts.inter(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 40.h),
+
+          Text(
+            'Date Range',
+            style: GoogleFonts.inter(
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          SizedBox(height: 10.h),
+          Text(
+            'Select the date range for which you want to see the mood chart.',
+            style: GoogleFonts.inter(
+              fontSize: 14.sp,
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(height: 24.h),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+
+              Flexible(
+                child: Obx(() => _buildDateField(
+                  'From',
+                  controller.fromDate.value,
+                      () => controller.selectFromDate(context),
+                )),
+              ),
+
+              SizedBox(width: 10.w),
+
+              Flexible(
+                child: Obx(() => _buildDateField(
+                  'To',
+                  controller.toDate.value,
+                      () => controller.selectToDate(context),
+                )),
+              ),
+
+            ],
+          ),
+
+          SizedBox(height: 220.h),
+
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () => controller.applyFilters(),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(vertical: 12.h),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.r),
+                ),
+              ),
+              child: Text(
+                'Apply',
+                style: GoogleFonts.inter(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 4.h),
+
+          Align(
+            alignment: Alignment.center,
+            child: TextButton(
+              onPressed: () => Get.back(),
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 16.h),
+                textStyle: const TextStyle(decoration: TextDecoration.underline),
+              ),
+              child: Text(
+                'Cancel',
+                style: GoogleFonts.inter(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDateField(String label, DateTime? date, VoidCallback onTap) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.h),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey[300]!),
+              borderRadius: BorderRadius.circular(24.r),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  date != null
+                      ? DateFormat('dd MMM yyyy').format(date)
+                      : label,
+                  style: GoogleFonts.inter(
+                    fontSize: 14.sp,
+                    color: date != null ? Colors.black : Colors.grey,
+                  ),
+                ),
+                SvgPicture.asset(
+                  datePickerIcon,
+                  width: 20.w,
+                  height: 20.h,
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
